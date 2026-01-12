845fb3f7677206129edb79e2e6571e786fd13702
Seriki yakub <sandraisom00@gmail.com>
2026-01-12 02:11:16 +0100

    Add files via upload

---
+++ Ytext.mk
@@ -0,0 +1,15 @@
+<!-- Default Statcounter code for paperweb http://Aura.web -->
+<script type="text/javascript">
+var sc_project=13197100; 
+var sc_invisible=0; 
+var sc_security="e9851b09"; 
+var scJsHost = "https://";
+document.write("<sc"+"ript type='text/javascript' src='" + scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
+</script>
+<noscript><div class="statcounter"><a title="Web Analytics"
+href="https://statcounter.com/" target="_blank"><img class="statcounter"
+src="https://c.statcounter.com/13197100/0/e9851b09/0/" alt="Web Analytics"
+referrerPolicy="no-referrer-when-downgrade"></a></div></noscript>
+<!-- End of Statcounter Code -->
+<a href="https://statcounter.com/p13197100/?guest=1">Check traffic</a>
